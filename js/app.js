8178a25ca9429b376fd1dca07adc4e9ddaa83567
OsmanRana <osmancmc@yahoo.com>
2021-09-13 00:34:50 +0600

    somebug fixed

--- js/app.js
+++ js/app.js
@@ -8,9 +8,15 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+  console.log(products);
   const allProducts = products.map((pd) => pd);
+  
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
+    const totalRating = product.rating.count;
+    console.log(totalRating);
+    const averageRating = product.rating.rate;
+    console.log(averageRating);
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -41,17 +47,18 @@ const getInputValue = (id) => {
   return converted;
 };
 
+
 // main price update function
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -70,11 +77,15 @@ const updateTaxAndCharge = () => {
     setInnerText("total-tax", priceConverted * 0.4);
   }
 };
+console.log(getInputValue('price'));
+console.log(getInputValue('delivery-charge'));
+console.log(getInputValue('total-tax'));
 
-//grandTotal update function
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
+    console.log(grandTotal);
   document.getElementById("total").innerText = grandTotal;
 };
+updateTotal();
